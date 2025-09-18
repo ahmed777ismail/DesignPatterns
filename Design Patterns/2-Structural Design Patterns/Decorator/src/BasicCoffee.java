3d054091521700e75543c4097eccfca081da10cd
ahmed777ismail <ahmed1kaball@gmail.com>
2025-09-18 09:06:48 +0300

    done

---
+++ Design Patterns/2-Structural Design Patterns/Decorator/src/BasicCoffee.java
@@ -0,0 +1,9 @@
+public class BasicCoffee implements Coffee{
+
+    public String getDescription() {
+        return "Basic Coffee";
+    }
+    public double getCost() {
+        return 10.0;
+    }
+}
---
+++ Design Patterns/2-Structural Design Patterns/Decorator/src/SugarDecorator.java
@@ -0,0 +1,11 @@
+class SugarDecorator extends CoffeeDecorator {
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+    public String getDescription() {
+        return coffee.getDescription() + ", Sugar";
+    }
+    public double getCost() {
+        return coffee.getCost() + 1.0;
+    }
+}
